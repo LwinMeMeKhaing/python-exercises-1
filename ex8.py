aaa7564a7339b9ee191003ba991b1dd7986885fb
Ba Nyar Naing <banyarnaing123@gmail.com>
2018-06-05 13:56:33 +0630

    update ex8.py

--- ex8.py
+++ ex8.py
@@ -1,10 +1,10 @@
-formatter = "%r %r %r %r"
+formatter = "{} {} {} {}"
 
-print(formatter % (1,2,3,4))
-print(formatter % ("One","two","three", "four"))
-print(formatter % (True, False, False, True))
-print(formatter % (formatter, formatter, formatter, formatter))
-print(formatter % (
+print(formatter.format(1,2,3,4))
+print(formatter.format("One","two","three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
     "I had this thing.",
     "That you could type up right.",
     "But it didn't sing.",
